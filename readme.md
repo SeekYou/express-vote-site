26fca6e1913970199774f27b4e4bca8f369990b9
DESKTOP-Q5ASHFU\weber <danaipengpeng@163.com>
2017-08-01 18:34:05 +0800

    修改express 413 报错，添加getById

--- readme.md
+++ readme.md
@@ -6,3 +6,17 @@
     "nodemon": "^1.11.0"
   }
 ```
+
+使用命令`nodemon`监听代码变化
+
+注释掉了route.all('*',function(req,res,next){
+	// res.header("Content-Type", "application/json");
+	})
+不然ejs会返回xml
+
+find({'_id': ""})
+
+req.query
+
+如果超出express最大请求，在app.js中加
+// app.use(bodyParser.json({"limit": "10000kb"}));
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var Sign = require('../models/signInfo.js');
 
 router.all('*',function(req,res,next){
+	console.log(req);
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("Access-Control-Expose-Headers", "*");
     res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json");
+    // res.header("Content-Type", "application/json");
     next();
 })
 
@@ -24,11 +25,23 @@ router.get('/api/getResult',function(req,res,next){
 		res.json(err)
 	})
 })
+router.get('/api/getById',function(req,res,next){
+	console.log(req.query.userId);
+	var userId=req.query.userId;
+	Sign.find({'_id': userId}).then(signInfo => {
+		console.log('success find');
+		res.json({result:signInfo})
+	})
+	.catch(err => {
+		res.json(err)
+	})
+})
 
 router.post('/new_post',function(req,res){
 	// console.log(req.originalUrl);
 	// console.log(req.url);
 	// console.log(req.body);
+	console.log(req);
 	Sign.create(req.body,(err,signInfo) => {
 		if (err) {
 			console.log(err);
